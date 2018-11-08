89e27b85be9457214faa9eb49e826ca975ba9fdd
= <=>
2018-11-08 21:51:15 +0700

    fix touch and button

--- core/src/com/tetris8bit/game/Assets/Button.java
+++ core/src/com/tetris8bit/game/Assets/Button.java
@@ -7,25 +7,25 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 public class Button {
-    public Texture textureOnClick;
-    public Texture textureOnRelease;
-    public Vector2 texturePosition;
-    public Vector2 texturePositionCenter;
+    private Texture textureOnClick;
+    private Texture textureOnRelease;
+    private Vector2 texturePosition;
+    private Vector2 texturePositionCenter;
     public boolean isClick; // khi button duoc click
     public boolean isSound; // cho phep co tieng hay ko khi click
-    public Button(String _textureOnClick,String _textureOnRelease,Vector2 _texturePositionCenter){
+    public boolean isCircle;
+    public Button(String _textureOnClick,String _textureOnRelease,Vector2 _texturePositionCenter,boolean _isCircle){
         textureOnClick=new Texture(_textureOnClick);
         textureOnRelease=new Texture(_textureOnRelease);
         texturePosition=new Vector2();
         texturePositionCenter=new Vector2();
-
-
+        isCircle=_isCircle;
         this.texturePositionCenter.set(_texturePositionCenter.x,_texturePositionCenter.y);
         this.texturePosition.set(texturePositionCenter.x-textureOnClick.getWidth()/2.0f,texturePositionCenter.y-textureOnClick.getHeight()/2.0f);
     }
 
-    public boolean checkOnClick(boolean isCircle){
-        if (GameTouch.istouch) {
+    public boolean checkOnClick(){
+        if (GameTouch.IsTouch()) {
             if (isCircle) {  // neu button co dang hinh tron
                 // xac dinh toa do tam hinh tron
                 // tinh toan xem toa do touch co nam trong hinh tron hay khong
@@ -45,8 +45,8 @@ public class Button {
         return false;
     }
 
-    public void render(SpriteBatch batch,boolean isCircle){
-        isClick=checkOnClick(isCircle);
+    public void render(SpriteBatch batch){
+        isClick=checkOnClick();
         batch.begin();
         if (isClick){
             batch.draw(textureOnClick,texturePosition.x,texturePosition.y);
@@ -54,10 +54,17 @@ public class Button {
         else {
             batch.draw(textureOnRelease,texturePosition.x,texturePosition.y);
         }
+        if(isSound){
+
+        }
+        else {
+
+        }
         batch.end();
     }
 
     public void dipose(){
-
+        textureOnClick.dispose();
+        textureOnRelease.dispose();
     }
 }
